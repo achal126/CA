ce4f282209c17eb0c21c9addc5635587b1034fc2
root <root@DESKTOP-L4MNKSO.localdomain>
2018-04-02 01:55:00 -0400

    DEMO 1

--- truffle.js
+++ truffle.js
@@ -7,6 +7,7 @@ module.exports = {
       host: 'localhost',
       port: 8545,
       gas: 6600000,
+from:'0x68a2819bd9039ed91d8a73f0b0477bd25d06a587',
       network_id: '*' // Match any network id
     }
   }
